3af3602d844342efaf776550c8f1bc7c120ec3b9
Samuel Borraccini <samuel@MacBook-Air-de-Samuel.local>
2024-04-30 19:00:32 -0400

    add chart

---
+++ app/charts/page.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import React from "react";
+import bravesLogo from "../../public/images/braves.png";
+import chart from "../../public/images/chart.png";
+const page = () => {
+  return (
+    <div>
+      <div className="justify-center items-center  flex flex-col space-y-2 mb-8">
+        <h1 className="text-4xl font-bold">View how your team is performing</h1>
+        <p className="text-white font-light">Check real time data</p>
+      </div>
+      <div className="flex space-x-6 mx-6">
+        <div className="flex-1 flex flex-col border-r-[1px] border-gray-700">
+          <h2 className="text-2xl font-bold mb-4">Real time data</h2>
+          <p>
+            Our IA update charts in realtime so you can get the latest
+            information about how a team is performing and can take actin
+            accurately and efficently
+          </p>
+          <div className="flex flex-col items-center justify-center mt-8">
+            <h2 className="font-bold text-green-500">Batting Average</h2>
+            <Image
+              className="mr-12"
+              src={bravesLogo}
+              width={200}
+              alt="team-logo"
+            />
+          </div>
+        </div>
+        <div className="flex-1 justify-center items-center flex">
+          <Image src={chart} width={400} alt="team-logo" />
+        </div>
+      </div>
+      <div className="mt-8 flex justify-center">
+        <ul className="flex justify-between space-x-9 px-12 py-4 w-80 rounded-full bg-gray-200 font-thin text-gray-500 ">
+          <li className="hover:text-black cursor-pointer">Avg</li>
+          <li className="hover:text-black cursor-pointer">Hits</li>
+          <li className="hover:text-black cursor-pointer">Runs</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default page;
